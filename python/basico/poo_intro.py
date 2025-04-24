7abc0c9cb9f0ed63832d53c8b9fc23cf59664713
matiasBulacio <142853542+matiasBulacio@users.noreply.github.com>
2025-04-24 00:44:27 +0000

    feito

--- python/basico/poo_intro.py
+++ python/basico/poo_intro.py
@@ -6,15 +6,24 @@ class Player:
     def __init__(self, nome, arma):
         self.nome = nome # autoriza modificação/objeto
         self.arma = arma
+    # Getter e Setter (fake)
+    #Método que intercepta a visualização
+    def get_nome(self):
+        print(f"Nome: ${self.nome}")
+    # Setter (fake)
+    def set_nome(it, nome):
+        it.nome = nome
 
-class Player2:
-    def __init__(self, nome, arma):
-        self.nome = nome # autoriza modificação/objeto
-        self.arma = arma
+# Aquiles: Player = Player('Aquiles', 'faca')
+# print(Aquiles.host, Aquiles.arma)
 
 kratos: Player = Player('Kratos', 'blades of chaos')
-print(kratos.nome, kratos.arma)
+print(kratos.set_nome("vivaldi"))
+print(kratos.get_nome())
+# print(kratos.nome, kratos.arma)
 
-dante: Player2 = Player2('Dante','oblivion')
-print(dante.nome, dante.arma)
+# dante: Player = Player('Dante','oblivion')
+# print(dante.nome, dante.arma)
+
+
 
